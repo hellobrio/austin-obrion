293629f64f681a5c1e3e16bdcd09ea4c369303bb
zeeshan <zeeshanweb.official@gmail.com>
2024-03-27 02:41:06 +0500

    image path updated

--- .vuepress/SEO.js
+++ .vuepress/SEO.js
@@ -5,7 +5,7 @@ const schema = {
     "url": "https://austinobrion.com",
     "jobTitle": "CRO/CMO, Founder, Growth Advisor",
     "gender": "male",
-    "image": "https://your-profile-image.com/image.jpg",
+    "image": "https://austinobrion.com/assets/img/img_portrait_austin_600x600.dc8f0657.jpg",
     "sameAs": [
       "https://www.linkedin.com/in/austinobrion/",
       "https://www.crunchbase.com/person/austin-o-brion",
@@ -13,7 +13,6 @@ const schema = {
       "https://www.f6s.com/member/austinobrion",
       "https://peerlist.io/austin/",
     ],
-    "datePublished": "2018-03-10",
     "description": "Your Partner in B2B SaaS Success. As a Fractional CMO, CRO, Expert & Growth Advisor, I craft tailored strategies for exponential growth."
   };
 
