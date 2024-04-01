ba2d735eb56cfc09eb42fa59de87568bfb872c8e
zeeshan <zeeshanweb.official@gmail.com>
2024-04-02 01:35:51 +0500

    updated code

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,14 +2,14 @@ const { path } = require('@vuepress/shared-utils')
 const schemaScript = require('./SEO')
 
 module.exports = {
-  title: "Austin O'Brion — B2B SaaS CRO, Founder, Growth Advisor",
+  // title: "Austin O'Brion — B2B SaaS CRO, Founder, Growth Advisor",
   description: "Austin O'Brion: Your Partner in B2B SaaS Success. As a Fractional CMO, CRO Expert & Growth Advisor, I craft tailored strategies for exponential growth. Let's ignite your journey now!",
   themeConfig: {
   },
   head: [
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap' }],
     [`script`, { type: 'application/ld+json' }, schemaScript],
-    // ['title', null, 'Austin O\'Brion — B2B SaaS CRO, Founder, Growth Advisor'],
+    ['title', null, 'Austin O\'Brion — B2B SaaS CRO, Founder, Growth Advisor'],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }]
   ],
   alias: {
