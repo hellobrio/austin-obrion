b0f3b0690adde6ede515c330d065dad2b54eb59e
zeeshan <zeeshanweb.official@gmail.com>
2024-05-04 07:49:28 +0500

    latest changes

--- .vuepress/theme/components/Connect.vue
+++ .vuepress/theme/components/Connect.vue
@@ -9,10 +9,11 @@
                         <p class="text-sm md:text-2xl">LINKEDIN</p>  
                     </a>
                 </div>
+                
                   <div class="h-32 md:h-64 w-32 md:w-64 rounded-2xl bg-connect flex flex-col justify-center items-center">
                     <a href="https://austinobrion.medium.com/" class="flex flex-col justify-center items-center">
                         <img src="../../public/images/icon_medium.svg" class="w-2/3" alt="">
-                        <p class="text-sm md:text-2xl">Medium Blog</p>  
+                        <p class="text-sm md:text-2xl">MEDIUM BLOG</p>  
                     </a>
                 </div>
                   <div class="h-32 md:h-64 w-32 md:w-64 rounded-2xl bg-connect flex flex-col justify-center items-center">
